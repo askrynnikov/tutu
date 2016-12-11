a835c83bd1ea0daebabbaccb3f0fd9522f6c0b64
askrynnikov <5101233@gmail.com>
2016-12-11 13:33:55 +0300

    Fix errors

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,8 +1,8 @@
 class RailwayStation < ApplicationRecord
   validates :title, presence: true
-  has_many :trains
-  has_many :tickets_initial_station, class_name:Ticket
-  has_many :tickets_final_station, class_name:Ticket
+  has_many :trains, class_name: Train, foreign_key: 'current_station_id'
+  has_many :tickets_initial_station, class_name: Ticket, foreign_key: 'initial_station_id'
+  has_many :tickets_final_station, class_name: Ticket, foreign_key: 'final_station_id'
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 end
