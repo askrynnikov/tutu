6d9ac69c980e1093e2e87c802c32c1862b836863
askrynnikov <5101233@gmail.com>
2016-12-23 20:04:38 +0300

    Fix bug

--- app/models/seat_car.rb
+++ app/models/seat_car.rb
@@ -1,6 +1,6 @@
 class SeatCar < Car
   NIL_PLACES = %i(top_places lower_places top_side_places lower_side_places).freeze
-  # validates :seat_places, presence: true, numericality: { only_integer: true }
+  validates :seat_places, presence: true, numericality: { only_integer: true }
 
   before_validation :set_nil
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :routes
   resources :trains do
     resources :cars, shallow: true
+    resources :compartment_cars, controller: 'cars', type: 'CompartmentCar', shallow: true
+    resources :seat_cars, controller: 'cars', type: 'SeatCar', shallow: true
+    resources :economy_cars, controller: 'cars', type: 'EconomyCar', shallow: true
+    resources :sleeping_cars, controller: 'cars', type: 'SleepingCar', shallow: true
   end
   resources :railway_stations do
     patch :update_station_order_arrival_departure, on: :member
