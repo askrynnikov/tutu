d44ed313f85a65b24113208174ceba8eebc87cb1
askrynnikov <5101233@gmail.com>
2016-12-29 21:02:27 +0300

    Lesson 18
    При регистрации пользователя запрашивать его имя и фамилию и после входа, выводить "Привет, <имя пользователя>!"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,19 @@ class ApplicationController < ActionController::Base
       search_path
     end
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+
+    # registration_params = [ :email, :first_name, :last_name, :password, :password_confirmation ]
+    # devise_parameter_sanitizer.for(:sign_up) {
+    #   |u| u.permit(registration_params)
+    # }
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :password, :password_confirmation])
+  end
 end
