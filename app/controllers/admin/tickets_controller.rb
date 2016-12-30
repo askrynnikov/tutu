8172c29fbd51f95e1118ffbdf94f0008287856fe
askrynnikov <5101233@gmail.com>
2016-12-30 14:57:06 +0300

    Lesson 18
    Администратор может просматривать список всех билетов в системе, каждый в отдельности, редактировать билеты и удалять их.

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -1,34 +1,49 @@
 class Admin::TicketsController < Admin::BaseController
   before_action :hidden_params, only: [:new]
-  before_action :set_ticket, only: [:show]
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def show
   end
 
   def create
-    # @ticket = Ticket.new(ticket_params)
-    @ticket = current_user.tickets.new(ticket_params)
+    @ticket = Ticket.new(ticket_params)
     if @ticket.save
-      redirect_to @ticket
+      redirect_to [:admin, @ticket], notice: 'Ticket was successfully created.'
     else
       render :new
     end
   end
 
   def new
-    # @ticket = Ticket.new(train_id: params[:train_id], start_station_id: params[:start_station_id], end_station_id: params[:end_station_id])
-    # не забывать про возможности slice в Rails
-    # @ticket = Ticket.new(params.slice(:train_id, :start_station_id, :end_station_id))
-    # @ticket = current_user.tickets.new(params.slice(:train_id, :start_station_id, :end_station_id))
-    @ticket = current_user.tickets.new(train_id: params[:train_id],
-                                       start_station_id: params[:start_station_id],
-                                       end_station_id: params[:end_station_id])
+    @ticket = Ticket.new(train_id: params[:train_id],
+                         start_station_id: params[:start_station_id],
+                         end_station_id: params[:end_station_id])
+  end
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      redirect_to [:admin, @ticket], notice: 'Ticket was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ticket.destroy
+    redirect_to admin_tickets_url, notice: 'Ticket was successfully destroyed.'
   end
 
   private
 
   def ticket_params
-    params.require(:ticket).permit(:train_id, :start_station_id, :end_station_id, :passenger_name, :passenger_passport)
+    params.require(:ticket).permit(:user_id, :train_id, :start_station_id, :end_station_id, :passenger_name, :passenger_passport)
   end
 
   def hidden_params
