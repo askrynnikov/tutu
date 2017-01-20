2709e69ec67b1c5dda285560679d15dbdfaf8b24
askrynnikov <5101233@gmail.com>
2017-01-20 12:50:30 +0300

    Lesson 20 before deploy

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,4 +6,14 @@ class Ticket < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :start_station, class_name: RailwayStation
   belongs_to :end_station, class_name: RailwayStation
+
+  after_create :send_notification
+
+  def route_name
+    "#{start_station.title} - #{end_station.title}"
+  end
+
+  def send_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
 end
